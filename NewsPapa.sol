03d1c59a8a8a9bfe284bc9289669f4c2783c70e8
bulengerkacper <bulenger.kacper@gmail.com>
2022-02-03 19:57:14 +0100

    added paying for news

--- NewsPapa.sol
+++ NewsPapa.sol
@@ -6,6 +6,7 @@ contract NewsPaper {
     string news_paper_name;
     uint creation_date;
     uint static_news_id;
+    address news_paper_owner;
 
     struct News {
         uint id;
@@ -21,6 +22,7 @@ contract NewsPaper {
         news_paper_name = _news_paper_name;
         creation_date = block.timestamp;
         static_news_id=0;
+        news_paper_owner = msg.sender;
     }
 
     function create_news(bytes memory _title, bytes memory _content) public {
@@ -28,14 +30,15 @@ contract NewsPaper {
         news.push(_inject);
     }
 
-    function reward_news_creator(uint _how_many_coins, uint _news_id) public {
+    function reward_news_creator(uint _news_id) public payable {
             address _news_owner;
             for (uint i=0; i < news.length; i++) {
-                if(news[i].id=_news_id) {
-                    _news_owner=news[id].address;
+                if(news[i].id==_news_id) {
+                    _news_owner=news[i].owner;
                 }
             }
-            payable(_news_owner).transfer(_how_many_coins);
+            payable(_news_owner).transfer(msg.value*99/100);
+            payable(news_paper_owner).transfer(msg.value*1/100);
     }
 }
 
