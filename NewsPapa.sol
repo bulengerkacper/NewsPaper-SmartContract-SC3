f63b0a4510aa4831e5294e390ec5f350a5ca2ab6
bulengerkacper <bulenger.kacper@gmail.com>
2022-02-03 19:49:07 +0100

    added rewarding

--- NewsPapa.sol
+++ NewsPapa.sol
@@ -28,5 +28,21 @@ contract NewsPaper {
         news.push(_inject);
     }
 
-
+    function reward_news_creator(uint _how_many_coins, uint _news_id) public {
+            address _news_owner;
+            for (uint i=0; i < news.length; i++) {
+                if(news[i].id=_news_id) {
+                    _news_owner=news[id].address;
+                }
+            }
+            payable(_news_owner).transfer(_how_many_coins);
+    }
 }
+
+/*
+TODOS: 
+-voting and giving money to post author
+-comments sytem !? - maybe should be based not on struct
+
+
+*/
