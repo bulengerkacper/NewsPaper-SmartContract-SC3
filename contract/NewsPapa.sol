55fe47ddc8cb7b6cf49157bf657a8145dc40ab70
bulengerkacper <bulenger.kacper@gmail.com>
2022-02-07 22:24:34 +0100

    added posibilit to push wei during creating news and comment

--- contract/NewsPapa.sol
+++ contract/NewsPapa.sol
@@ -5,8 +5,8 @@ contract NewsPaper {
 
     string news_paper_name;
     uint creation_date;
-    uint private static_news_id;
-    uint private static_comment_id; 
+    uint256 private static_news_id;
+    uint256 private static_comment_id; 
     address news_paper_owner;
 
     struct News {
@@ -35,14 +35,15 @@ contract NewsPaper {
         news_paper_owner = msg.sender;
     }
 
-    function create_news(string memory _title, string memory _content) public {
+    function create_news(string memory _title, string memory _content) public payable {
         News memory _inject = News(++static_news_id, address(msg.sender), block.timestamp, _title, _content);
         news.push(_inject);
+        payable(news_paper_owner).transfer(msg.value);
     }
 
     function reward_news_creator(uint _news_id) public payable {
         address _news_owner;
-        for (uint i=0; i < news.length; i++) {
+        for (uint256 i=0; i < news.length; i++) {
             if(news[i].id ==_news_id) {
                 _news_owner = news[i].owner;
                 break;
@@ -53,14 +54,15 @@ contract NewsPaper {
     }
 
 
-    function create_comment(uint _id_of_news, string memory _message, string memory _pseudonim) public {
+    function create_comment(uint _id_of_news, string memory _message, string memory _pseudonim) public payable {
         Comment memory _inject = Comment(++static_comment_id, _id_of_news, msg.sender, _message,_pseudonim);
         comments.push(_inject);
+        payable(news_paper_owner).transfer(msg.value);
     }
 
     function reward_comment_creator(uint _comment_id) public payable {
         address _comment_owner;
-        for(uint i=0; i < comments.length; i++) {
+        for(uint256 i=0; i < comments.length; i++) {
             if(comments[i].comment_id == _comment_id) {
                 _comment_owner = comments[i].owner;
                 break;
@@ -83,6 +85,7 @@ contract NewsPaper {
 
 /*
 TODOS: 
+- rewrite to u256
 -comments sytem !? - maybe should be based not on smart contract - during consideration
 -counting awards
 */
