e6ab3030268f01f3c3c8afa64cbeb4adc2bc051a
bulengerkacper <bulenger.kacper@gmail.com>
2022-09-26 10:09:19 +0200

    changes in content of contract

--- contracts/NewsPaper.sol
+++ contracts/NewsPaper.sol
@@ -3,10 +3,8 @@ pragma solidity ^0.8.11;
 
 contract NewsPaper {
 
-    string news_paper_name;
-    uint creation_date;
-    uint256 private static_news_id;
-    uint256 private static_comment_id; 
+    uint256 static_news_id;
+    uint256 static_comment_id; 
     address news_paper_owner;
 
     struct News {
@@ -25,83 +23,51 @@ contract NewsPaper {
         string pseudonim;
     }
 
-    News[] public news;
-    Comment[] public comments;
+    mapping(uint256 => News) news;
+    mapping(uint256 => Comment) comments;
 
-    constructor(string memory _news_paper_name) {
-        news_paper_name = _news_paper_name;
-        creation_date = block.timestamp;
+    constructor() {
         static_news_id = 0; 
+        static_comment_id = 0;
         news_paper_owner = msg.sender;
     }
 
     function create_news(string calldata _title, string calldata _content) public payable {
         News memory _inject = News(++static_news_id, address(msg.sender), block.timestamp, _title, _content);
-        news.push(_inject);
+        news[static_news_id] = _inject;
         payable(news_paper_owner).transfer(msg.value);
     }
 
     function reward_news_creator(uint _news_id) public payable {
-        address _news_owner;
-        for (uint256 i=0; i < news.length; i++) {
-            if(news[i].id ==_news_id) {
-                _news_owner = news[i].owner;
-                break;
-            }
-        }
-        payable(_news_owner).transfer(msg.value * 99/100);
+        payable(news[_news_id].owner).transfer(msg.value * 99/100);
         payable(news_paper_owner).transfer(msg.value * 1/100);
     }
 
 
     function create_comment(string calldata _message, string calldata _pseudonim,uint256 _id_of_news) public payable {
         Comment memory _inject = Comment(++static_comment_id, _id_of_news, msg.sender, _message,_pseudonim);
-        comments.push(_inject);
+        comments[static_comment_id] = _inject;
         payable(news_paper_owner).transfer(msg.value);
     }
 
     function reward_comment_creator(uint _comment_id) public payable {
-        address _comment_owner;
-        for(uint256 i=0; i < comments.length; i++) {
-            if(comments[i].comment_id == _comment_id) {
-                _comment_owner = comments[i].owner;
-                break;
-            }
-        }
-        payable(_comment_owner).transfer(msg.value*99/100);
+        payable(comments[_comment_id].owner).transfer(msg.value*99/100);
         payable(news_paper_owner).transfer(msg.value * 1/100);
     }
-
-    function get_current_news_id() public view returns (uint256) {
-        return static_news_id;
-    }
-
-    function get_current_commend_id() public view returns (uint256) {
-        return static_comment_id;
-    }
-
 
     modifier only_admin {
 		require(msg.sender == news_paper_owner," you are not an admin");
 		_;
 	}
 
-    function red_button() public only_admin {
-        delete news;
-        delete comments;
-    }
-
     function remove_news(uint256 _id) public only_admin {
-        delete news[_id];
+        news[_id].content="";
+        news[_id].title="";
+
     }
 
     function remove_comment(uint256 _id) public only_admin {    
-        delete comments[_id];
+        comments[_id].message="";
+        comments[_id].pseudonim="";
     }
 }
-
-/*
-TODOS: 
-- rewrite to u256
-- anti multiple comments in same time system
-*/
